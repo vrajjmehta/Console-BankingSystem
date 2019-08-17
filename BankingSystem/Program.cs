8d81a121d804b5860d4bf99bf6aade9d1c7dc9c0
Vraj Mehta <vrajjmehta@gmail.com>
2019-08-17 16:17:00 +1000

    Version 0

---
+++ BankingSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
